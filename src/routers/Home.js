fed1677505a305fc3e4b7300f52dbd3eed9d1dd5
kajj8808 <71279997+kajj8808@users.noreply.github.com>
2021-08-08 01:46:13 +0900

    Update Home 2021-08-08
    Home Presenter add

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import styled from 'styled-components';
+import Movie from '../components/Movie';
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
 
 const Title = styled.h1`
+    color: #dfe6e9;
     font-weight: 600;
-    color : #dfe6e9;
-    font-size: 18px;
+    font-size: 30px;
+`;
+
+const Subtitle = styled.h2`
+    color: #dfe6e9;
+    font-weight: 400;
+    font-size: 13px;
 `;
 
 const Header = styled.header`
@@ -16,8 +28,10 @@ const Header = styled.header`
     height: 200px;
     background: linear-gradient(to left, blue, pink);
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    gap: 8px;
 `;
 
 const getMovies = gql`
@@ -30,20 +44,46 @@ const getMovies = gql`
     }
 `;
 
-export default () => {
+const MovieContainer = styled.div`
+    position: relative;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    width: 95%;
+    gap: 25px;
+    top: -30px;
+`;
+
+const Loading = styled.h3`
+    position : relative;
+    top : 15px;
+    color : #bdc3c7;
+`;
+
+const HomePage = () => {
     const { loading, error, data } = useQuery(getMovies);
     return (
         <Container>
             <Header>
-                <Title>Header</Title>
+                <Title>Apollo 2021</Title>
+                <Subtitle>GraphQL</Subtitle>
             </Header>
-            {loading
-                ? 'loading...'
-                : data.movies.map((movie) => (
-                      <>
-                          <h1>{movie.title}</h1>
-                      </>
-                  ))}
+            {loading ? (
+                <Loading>loading...</Loading>
+            ) : (
+                data.movies && (
+                    <MovieContainer>
+                        {data.movies.map((movie) => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                bg={movie.medium_cover_image}
+                            />
+                        ))}
+                    </MovieContainer>
+                )
+            )}
         </Container>
     );
 };
+
+export default HomePage;
